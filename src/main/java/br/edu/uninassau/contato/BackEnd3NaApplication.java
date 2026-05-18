10b330efdb421c40b0d74842beff394da5381e51
Pedro <pedrodiimacedo@gmail.com>
2026-05-18 19:43:12 -0300

    correcao de erros

--- src/main/java/br/edu/uninassau/contato/BackEnd3NaApplication.java
+++ src/main/java/br/edu/uninassau/contato/BackEnd3NaApplication.java
@@ -2,15 +2,28 @@ package br.edu.uninassau.contato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackEnd3NaApplication {
-		
-		public static void main(String[] args) {
-		SpringApplication.run(BackEnd3NaApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        SpringApplication.run(BackEnd3NaApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
 }
