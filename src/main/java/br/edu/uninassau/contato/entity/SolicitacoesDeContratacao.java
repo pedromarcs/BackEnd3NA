48bb38c9e04fbf01ec2dcc019d5840b340205931
caiovini100 <caiomqnvinicius100@gmail.com>
2026-05-25 19:36:45 -0300

    correção de bug

--- src/main/java/br/edu/uninassau/contato/entity/SolicitacoesDeContratacao.java
+++ src/main/java/br/edu/uninassau/contato/entity/SolicitacoesDeContratacao.java
@@ -14,12 +14,11 @@ public class SolicitacoesDeContratacao {
     @Column(name = "id_solicitacao")
     private Long idSolicitacao;
 
-    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "fk_id_user", nullable = false)
     private Usuario usuario;
 
-    @JsonIgnore
+    @JsonIgnore 
     @ManyToOne
     @JoinColumn(name = "fk_id_nutri", nullable = false)
     private Nutricionista nutricionista;
@@ -37,14 +36,19 @@ public class SolicitacoesDeContratacao {
 
     public Long getIdSolicitacao() { return idSolicitacao; }
     public void setIdSolicitacao(Long idSolicitacao) { this.idSolicitacao = idSolicitacao; }
+    
     public Usuario getUsuario() { return usuario; }
     public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+    
     public Nutricionista getNutricionista() { return nutricionista; }
     public void setNutricionista(Nutricionista nutricionista) { this.nutricionista = nutricionista; }
+    
     public LocalDate getDataSolicitacao() { return dataSolicitacao; }
     public void setDataSolicitacao(LocalDate dataSolicitacao) { this.dataSolicitacao = dataSolicitacao; }
+    
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
+    
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 }
